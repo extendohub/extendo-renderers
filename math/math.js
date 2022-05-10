ea1f70ac04bf1b79b2753b24a0d53216204eed76
Jeff McAffer <jeffmcaffer@github.com>
2022-05-10 14:19:15 -0700

    undo url hack

--- math/math.js
+++ math/math.js
@@ -11,10 +11,6 @@ const { RegisterHTMLHandler } = _RegisterHTMLHandler
 import _AllPackages from 'mathjax-full/js/input/tex/AllPackages.js'
 const { AllPackages } = _AllPackages
 
-// TODO need to add this here as someone's using it and `ncc` seems not to bring that code in. 
-import URL from 'url'
-const foo = URL
-
 export default async ({ render }) => {
   const content = await render.getContent({}, 'utf8')
   const adaptor = liteAdaptor()
