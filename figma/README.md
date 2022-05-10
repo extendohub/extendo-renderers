122012ea6293d512826edc23d8503d45052424e8
Jeff McAffer <jeffmcaffer@github.com>
2022-05-09 19:42:08 -0700

    add figma nodefinder plugin

---
+++ figma/README.md
@@ -0,0 +1,2 @@
+The folder has the Figma plugin that helps to get node coordinates for Figma diagrams.
+Figma URLs only go down to shareable things like `frames`
---
+++ figma/plugin/nodeFinder/code.ts
@@ -0,0 +1,9 @@
+figma.showUI(__html__);
+
+figma.ui.onmessage = msg => {  if (msg.type === 'getSelection') {
+    const selection = figma.currentPage.selection
+    const page = figma.currentPage.id
+    const file = figma.fileKey
+    figma.ui.postMessage({ selection, page, file, sequence: msg.sequence })
+  }
+}
